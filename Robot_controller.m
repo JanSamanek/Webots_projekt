8dd33a49cc2e55a29826361a210dd1596725f327
JanSamanek <jan.d.samanek@gmail.com>
2020-11-30 11:17:59 +0100

    Update Robot_controller.m

--- Robot_controller.m
+++ Robot_controller.m
@@ -1,70 +1,23 @@
-% MATLAB controller for Webots
-% File:          my_controller.m
-% Date:
-% Description:
-% Author:
-% Modifications:
 
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
+  TIME_STEP = 64;
+%sensor settings  
+  l_sensor = wb_robot_get_device('light sensor')
+  d_sensor = wb_robot_get_device('d_sensor');
+  d_sensor_F_L = wb_robot_get_device('d_sensor_F_L');
+  d_sensor_F_R = wb_robot_get_device('d_sensor_F_R');
 
-TIME_STEP = 64;
-
-% get and enable devices, e.g.:
-%  camera = wb_robot_get_device('camera');
-%  wb_camera_enable(camera, TIME_STEP);
-% % get the motors and set target position to infinity (speed control)
-
-%left_front_motor = wb_robot_get_device('left_front_motor');
-%right_front_motor = wb_robot_get_device('right_front_motor');
-%left_back_motor = wb_robot_get_device('left_back_motor');
-%right_back_motor = wb_robot_get_device('right_back_motor');
-
-d_sensor = wb_robot_get_device('d_sensor');
-
-d_sensor_F_L = wb_robot_get_device('d_sensor_F_L')
-d_sensor_F_R = wb_robot_get_device('d_sensor_F_R')
-
-%lidar = wb_robot_get_device('lidar')
-
-
-%wb_motor_set_position(left_front_motor, inf);
-%wb_motor_set_velocity(left_front_motor, 5);
-
-%wb_motor_set_position(right_front_motor, inf);
-%wb_motor_set_velocity(right_front_motor, 5);
-
-%wb_motor_set_position(left_back_motor, inf);
-%wb_motor_set_velocity(left_back_motor, 5);
-
-%wb_motor_set_position(right_back_motor, inf);
-%wb_motor_set_velocity(right_back_motor, 5);
-
-wb_distance_sensor_enable(d_sensor, TIME_STEP);
-wb_distance_sensor_enable(d_sensor_F_L, TIME_STEP);
-wb_distance_sensor_enable(d_sensor_F_R, TIME_STEP);
-
-%period = wb_lidar_get_sampling_period(lidar)
-%wb_lidar_enable(lidar, TIME_STEP);
-%wb_gps_enable(gps, TIME_STEP);
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
+  wb_light_sensor_enable(l_sensor, TIME_STEP)
+  wb_distance_sensor_enable(d_sensor, TIME_STEP);
+  wb_distance_sensor_enable(d_sensor_F_L, TIME_STEP);
+  wb_distance_sensor_enable(d_sensor_F_R, TIME_STEP);
 
 while wb_robot_step(TIME_STEP) ~= -1
 
+%sensor values
+    data_l_sensor = wb_light_sensor_get_value(l_sensor)
     distance = wb_distance_sensor_get_value(d_sensor);
     distance_F_L = wb_distance_sensor_get_value(d_sensor_F_L);
     distance_F_R = wb_distance_sensor_get_value(d_sensor_F_R);
-    
-    
-  %    range = wb_lidar_get_range_image(lidar)
-    %min_range = wb_lidar_get_min_range(lidar)
-    %disp(range)
-  %  disp(period)
-
     
   %  wb_console_print(sprintf('%u\n',range), WB_STDOUT);
    % disp(distance)
@@ -73,123 +26,70 @@ while wb_robot_step(TIME_STEP) ~= -1
     wb_console_print(sprintf('%u\n',distance_F_R), WB_STDOUT);
     %disp(distance_F_L)
     wb_console_print(sprintf('%u\n',distance_F_L), WB_STDOUT);
+    
+%motor settings
+  left_front_motor = wb_robot_get_device('left_front_motor');
+  right_front_motor = wb_robot_get_device('right_front_motor');
+  left_back_motor = wb_robot_get_device('left_back_motor');
+  right_back_motor = wb_robot_get_device('right_back_motor');
+    
+    
+  wb_motor_set_position(left_front_motor, inf);
+  wb_motor_set_position(right_front_motor, inf);
+  wb_motor_set_position(left_back_motor, inf);
+  wb_motor_set_position(right_back_motor, inf);
+
+%main code  
+  orientace = 'foward';
 
 
-     if distance > 80
+ if distance > 80 
       if  distance_F_L > 995 && distance_F_R > 995 
-           vpred
-        else
-           if distance_F_L < 995        
-              right
-                
-           elseif distance_F_R < 995       
-              left
+          orientace = 'foward';
+       else          
+           if distance_F_L < 995       
+              orientace = 'right';
+           elseif distance_F_R < 995  
+              orientace = 'left';
            end
-        end
-      else
-           left   
-      end
-       
-      %  if  distance_F_R > 950
-      
-      %  vpred
-        
-      % else
-         
-          %     left
-   
-     %  end
-      
-        
-  % read the sensors, e.g.:
-  
-  %  rgb = wb_camera_get_image(camera);
+      end      
+  else
+   orientace = 'right';   
+  end
 
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
-end
-
-% cleanup code goes here: write data to files, etc.
-
-function vpred
-left_front_motor = wb_robot_get_device('left_front_motor');
-right_front_motor = wb_robot_get_device('right_front_motor');
-left_back_motor = wb_robot_get_device('left_back_motor');
-right_back_motor = wb_robot_get_device('right_back_motor');
-
-  wb_motor_set_position(left_front_motor, inf);
+% orientation controler
+switch orientace
+case 'foward'
   wb_motor_set_velocity(left_front_motor, 5);
-
-  wb_motor_set_position(right_front_motor, inf);
   wb_motor_set_velocity(right_front_motor, 5);
-
-  wb_motor_set_position(left_back_motor, inf);
   wb_motor_set_velocity(left_back_motor, 5);
-
-  wb_motor_set_position(right_back_motor, inf);
   wb_motor_set_velocity(right_back_motor, 5);
-end
 
-function stop
-left_front_motor = wb_robot_get_device('left_front_motor');
-right_front_motor = wb_robot_get_device('right_front_motor');
-left_back_motor = wb_robot_get_device('left_back_motor');
-right_back_motor = wb_robot_get_device('right_back_motor');
-
-  wb_motor_set_position(left_front_motor, inf);
+case 'stop'
   wb_motor_set_velocity(left_front_motor, 0);
-
-  wb_motor_set_position(right_front_motor, inf);
   wb_motor_set_velocity(right_front_motor, 0);
-
-  wb_motor_set_position(left_back_motor, inf);
   wb_motor_set_velocity(left_back_motor, 0);
-
-  wb_motor_set_position(right_back_motor, inf);
   wb_motor_set_velocity(right_back_motor, 0);
-end
 
-
-function right
-left_front_motor = wb_robot_get_device('left_front_motor');
-right_front_motor = wb_robot_get_device('right_front_motor');
-left_back_motor = wb_robot_get_device('left_back_motor');
-right_back_motor = wb_robot_get_device('right_back_motor');
-
-  wb_motor_set_position(left_front_motor, inf);
+case 'right'
   wb_motor_set_velocity(left_front_motor, 5);
-
-  wb_motor_set_position(right_front_motor, inf);
   wb_motor_set_velocity(right_front_motor, -5);
-
-  wb_motor_set_position(left_back_motor, inf);
   wb_motor_set_velocity(left_back_motor, 5);
+  wb_motor_set_velocity(right_back_motor, -5);
 
-  wb_motor_set_position(right_back_motor, inf);
+case 'left'
+  wb_motor_set_velocity(left_front_motor, -5);
+  wb_motor_set_velocity(right_front_motor, 5);
+  wb_motor_set_velocity(left_back_motor, -5);
+  wb_motor_set_velocity(right_back_motor, 5);
+
+case 'back'
+  wb_motor_set_velocity(left_front_motor, -5);
+  wb_motor_set_velocity(right_front_motor, -5);
+  wb_motor_set_velocity(left_back_motor, -5);
   wb_motor_set_velocity(right_back_motor, -5);
 end
 
-function left
-left_front_motor = wb_robot_get_device('left_front_motor');
-right_front_motor = wb_robot_get_device('right_front_motor');
-left_back_motor = wb_robot_get_device('left_back_motor');
-right_back_motor = wb_robot_get_device('right_back_motor');
-
-  wb_motor_set_position(left_front_motor, inf);
-  wb_motor_set_velocity(left_front_motor, -5);
-
-  wb_motor_set_position(right_front_motor, inf);
-  wb_motor_set_velocity(right_front_motor, 5);
-
-  wb_motor_set_position(left_back_motor, inf);
-  wb_motor_set_velocity(left_back_motor, -5);
-
-  wb_motor_set_position(right_back_motor, inf);
-  wb_motor_set_velocity(right_back_motor, 5);
 end
+
+
