c067ec9d11142f409b658bfa9041baa6c3dd5079
MirekKalis <74046022+MirekKalis@users.noreply.github.com>
2020-12-13 00:21:05 +0100

    Add files via upload

--- rizeni.m
+++ rizeni.m
@@ -1,16 +1,39 @@
-function smer = rizeni (distance, distance_F_L, distance_F_R)
+function smer = rizeni (distance, distance_F_L, distance_F_R, dira, barva)
 if distance > 80 
       if  distance_F_L > 995 && distance_F_R > 995 
           smer = 'forward';
        else          
-           if distance_F_L < 995       
+           if distance_F_L < 990      
               smer = 'right';
-         elseif distance_F_R < 995  
+         elseif distance_F_R < 990  
               smer = 'left';
            end
      end      
 else
-         smer = 'left';   
+    if dira == 1
+       smer = 'left';
+    end
+    if dira == 2
+       smer = 'right';
+    end
+    
+    if dira == 3
+        
+      
+        if barva == 1
+         smer = 'right';
+         
+         elseif barva == 2
+         smer = 'left';
+         
+         elseif barva == 3
+         smer = 'right';
+        
+         elseif barva == 4
+          smer = 'left'; 
+        end
+    end
+    
 end
 end
 
